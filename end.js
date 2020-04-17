75fa5f5a609c8e37d509dc59a7786dec089cf6b4
rzjrh <rzjrh@hotmail.com>
2020-04-16 22:17:42 -0500

    finalize the end page - highscores

--- end.js
+++ end.js
@@ -4,6 +4,12 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
@@ -11,4 +17,15 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     console.log("clicked the save button!");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    console.log(highScores);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("index.html");
 };
